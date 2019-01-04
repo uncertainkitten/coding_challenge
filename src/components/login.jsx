2f69a23d028aaf2b5be8ea6586603facfef59a5c
Cassandra <uncertainkitten@gmail.com>
2019-01-03 20:06:05 -0800

    Create login page with username persistence to local storage

---
+++ src/components/login.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {username: "", password: ""}
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    localStorage.setItem("username", this.state.username)
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="login-page">
+          <div className="login-container">
+            <form onSubmit={this.handleSubmit} className="login-form-box">
+              <p className = "login-label">Login</p>
+              <h3 className="auth-label">User Name</h3>
+                  <input type="text" value={this.state.username} onChange={this.update("username")} className="login-input" />
+              <h3 className="auth-label">Password</h3>
+                  <input type="password" value={this.state.password} onChange={this.update("password")} className="login-input" />
+              <div className="login-btn">Log In</div>
+              <input className="signup-submit" type="submit" value="Log in" />
+            </form>
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default LoginForm;
